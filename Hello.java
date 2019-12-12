85b2c79493dfb5b103e78cae5dc6203fd4427900
HongSeokJun <1861453@hansung.ac.kr>
2019-12-12 09:26:04 +0900

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world from GIT!");
             System.out.println("Changes after Forst commit!");
+                System.out.println("Chanfes after Pushing to remote");
     }
 }
